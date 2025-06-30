0f0bc100c974cf1e05e84de5842bbff5ef68ec81
Cholito-ofc <onanposadas7@gmail.com>
2025-06-29 20:06:33 -0600

    Actualizar GIF•Abrazar.js

--- plugins/GIF•Abrazar.js
+++ plugins/GIF•Abrazar.js
@@ -1,14 +1,14 @@
-const handler = async (msg, { conn, args }) => {
+const handler = async (msg, { conn }) => {
   const mentionedJid = msg.mentionedJid && msg.mentionedJid[0]
   const quotedJid = msg.quoted && msg.quoted.sender
   const target = mentionedJid || quotedJid
 
-  const sender = '@' + msg.sender.split('@')[0]
+  const senderId = msg.sender || 'desconocido@s.whatsapp.net'
+  const sender = '@' + senderId.split('@')[0]
   const receiver = target ? '@' + target.split('@')[0] : '¡alguien!'
 
   const texto = `🤗 ${sender} le dio un gran abrazo a ${receiver}`
 
-  // Lista de GIFs de abrazos (puedes agregar más enlaces si quieres)
   const gifs = [
     'https://media.giphy.com/media/l2QDM9Jnim1YVILXa/giphy.gif',
     'https://media.giphy.com/media/od5H3PmEG5EVq/giphy.gif',
@@ -16,14 +16,13 @@ const handler = async (msg, { conn, args }) => {
     'https://media.giphy.com/media/BXrwTdoho6hkQ/giphy.gif'
   ]
 
-  // Selecciona un GIF aleatorio
   const gif = gifs[Math.floor(Math.random() * gifs.length)]
 
   await conn.sendMessage(msg.chat, {
     video: { url: gif },
     gifPlayback: true,
     caption: texto,
-    mentions: [msg.sender, ...(target ? [target] : [])]
+    mentions: [senderId, ...(target ? [target] : [])]
   })
 }
 
