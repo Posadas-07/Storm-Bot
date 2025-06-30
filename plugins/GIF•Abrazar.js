15b1f404c1e315d6a6b09e6643c55b9c8bed3cad
Cholito-ofc <onanposadas7@gmail.com>
2025-06-29 20:15:38 -0600

    Actualizar GIF•Abrazar.js

--- plugins/GIF•Abrazar.js
+++ plugins/GIF•Abrazar.js
@@ -1,66 +1,50 @@
-// 🔱 Código adaptado al estilo Killua Bot por ChatGPT
-const fetch = require('node-fetch');
+// 🔱 Plugin estilo Killua Bot adaptado por ChatGPT
+const videos = [
+  'https://telegra.ph/file/6a3aa01fabb95e3558eec.mp4',
+  'https://telegra.ph/file/0e5b24907be34da0cbe84.mp4',
+  'https://telegra.ph/file/6bc3cd10684f036e541ed.mp4',
+  'https://telegra.ph/file/3e443a3363a90906220d8.mp4',
+  'https://telegra.ph/file/56d886660696365f9696b.mp4',
+  'https://telegra.ph/file/3eeadd9d69653803b33c6.mp4',
+  'https://telegra.ph/file/436624e53c5f041bfd597.mp4',
+  'https://telegra.ph/file/5866f0929bf0c8fe6a909.mp4'
+];
 
-module.exports = {
-  command: ['hug', 'abrazar'],
-  tags: ['anime'],
-  help: ['hug @tag', 'abrazar @tag'],
-  group: true,
-  premium: false,
-  owner: false,
-  admin: false,
+module.exports = async (msg, { conn, text, participants }) => {
+  const mentioned = msg.mentionedJid && msg.mentionedJid[0];
+  const quoted = msg.quoted ? msg.quoted.sender : null;
+  const who = mentioned || quoted || msg.sender;
 
-  async handler(msg, { conn, participants }) {
-    let who;
+  const name = await conn.getName(who);
+  const name2 = await conn.getName(msg.sender);
 
-    if (msg.mentionedJid.length > 0) {
-      who = msg.mentionedJid[0];
-    } else if (msg.quoted) {
-      who = msg.quoted.sender;
-    } else {
-      who = msg.sender;
+  await conn.sendMessage(msg.chat, {
+    react: {
+      text: '🤗',
+      key: msg.key
     }
+  });
 
-    const name = await conn.getName(who);
-    const name2 = await conn.getName(msg.sender);
-
-    // Emoji de reacción personalizado
-    await conn.sendMessage(msg.chat, {
-      react: {
-        text: '🤗',
-        key: msg.key
-      }
-    });
-
-    // Frase personalizada con markdown
-    let texto;
-    if (msg.mentionedJid.length > 0) {
-      texto = `*${name2}* le dio un fuerte abrazo a *${name}* 🫂`;
-    } else if (msg.quoted) {
-      texto = `*${name2}* abrazó a *${name}* 🫂`;
-    } else {
-      texto = `*${name2}* se abrazó a sí mismo 🥺`;
-    }
-
-    // Lista de videos tipo gif hug
-    const videos = [
-      'https://telegra.ph/file/6a3aa01fabb95e3558eec.mp4',
-      'https://telegra.ph/file/0e5b24907be34da0cbe84.mp4',
-      'https://telegra.ph/file/6bc3cd10684f036e541ed.mp4',
-      'https://telegra.ph/file/3e443a3363a90906220d8.mp4',
-      'https://telegra.ph/file/56d886660696365f9696b.mp4',
-      'https://telegra.ph/file/3eeadd9d69653803b33c6.mp4',
-      'https://telegra.ph/file/436624e53c5f041bfd597.mp4',
-      'https://telegra.ph/file/5866f0929bf0c8fe6a909.mp4'
-    ];
-
-    const video = videos[Math.floor(Math.random() * videos.length)];
-
-    await conn.sendMessage(msg.chat, {
-      video: { url: video },
-      gifPlayback: true,
-      caption: texto,
-      mentions: [who]
-    }, { quoted: msg });
+  let texto;
+  if (mentioned) {
+    texto = `*${name2}* le dio un fuerte abrazo a *${name}* 🫂`;
+  } else if (quoted) {
+    texto = `*${name2}* abrazó a *${name}* 🫂`;
+  } else {
+    texto = `*${name2}* se abrazó a sí mismo 🥺`;
   }
+
+  const video = videos[Math.floor(Math.random() * videos.length)];
+
+  await conn.sendMessage(msg.chat, {
+    video: { url: video },
+    gifPlayback: true,
+    caption: texto,
+    mentions: [who]
+  }, { quoted: msg });
 };
+
+module.exports.command = ['hug', 'abrazar'];
+module.exports.tags = ['anime'];
+module.exports.help = ['hug @tag', 'abrazar @tag'];
+module.exports.group = true;
